209c2f58a28826c884fdbff91181d3ac8a9856c3
jl52 <jl52@138.16.109.62>
2011-11-21 22:43:04 -0500

    SeqSSMDP.java implements MDP optimization of a sequential SPSB agent X_t.java is a class representing the states (X,t) in MDP

---
+++ src/X_t.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class X_t {
+	Set<Integer> X;
+	int t;
+	// This class implements the (X,t) pair in MDP iteration, where X is the set of goods obtained, and t is the time
+	public X_t (Set<Integer> X, int t){
+		this.X = X;
+		this.t = t;
+	}
+
+}
